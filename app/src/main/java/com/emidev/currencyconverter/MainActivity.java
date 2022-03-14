76a302514971f206b8e405eb5dd34489c53e9b4d
Emiliano Iacopini <emiliano.iacopini@studenti.galileilivorno.edu.it>
2022-03-14 15:37:23 +0100

    Upgraded app

--- app/src/main/java/com/emidev/currencyconverter/MainActivity.java
+++ app/src/main/java/com/emidev/currencyconverter/MainActivity.java
@@ -5,28 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     double currencyOne, currencyTwo;
     boolean isDollarsCurrencyOne = true;
 
+    //Metodo giusto per fare una funzione ?
+    //Si può/dovrebbe fare ?
+    private void convert (Spinner cOS, Spinner cTS, EditText cOET, EditText cTET){
+        String currencyOne = cOS.getSelectedItem().toString();
+        String currencyTwo = cTS.getSelectedItem().toString();
+        if(cOET.getText().toString().equals("")){
+            Toast toast = Toast.makeText(getApplicationContext(), R.string.Toast, Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        double amount = Double.parseDouble(cOET.getText().toString());
+
+        Locator loc = new Locator(currencyOne, currencyTwo, amount);
+
+        Thread request = new Thread(loc);
+
+        request.start();
+        try {
+            request.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        cTET.setText(String.format(java.util.Locale.US, "%.2f", loc.getConversion()));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Button swapButton = findViewById(R.id.swapButton);
-        Button convertButton = findViewById(R.id.convertButton);
-
         Spinner currencyOneSpinner = findViewById(R.id.currencyOneSpinner);
         Spinner currencyTwoSpinner = findViewById(R.id.currencyTwoSpinner);
+        EditText currencyOneEditText = findViewById(R.id.currencyOneEditText);
+        EditText currencyTwoEditText = findViewById(R.id.currencyTwoEditText);
+
+        Button convertButton = findViewById(R.id.convertButton);
 
         ArrayAdapter<CharSequence>adapterOne= ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
         adapterOne.setDropDownViewResource(android.R.layout.simple_spinner_item);
@@ -38,49 +67,21 @@ public class MainActivity extends AppCompatActivity {
 
         currencyTwoSpinner.setAdapter(adapterTwo);
 
-        //TextView currencyOneTextView = findViewById(R.id.currencyOneTextView);
-        //TextView currencyTwoTextView = findViewById(R.id.currencyTwoTextView);
-
-        EditText currencyOneEditText = findViewById(R.id.currencyOneEditText);
-        EditText currencyTwoEditText = findViewById(R.id.currencyTwoEditText);
-
-        /* swapButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v){
-                if(isDollarsCurrencyOne){
-                    //currencyOneTextView.setText(R.string.Euros);
-                    //currencyTwoTextView.setText(R.string.Dollars);
-                    isDollarsCurrencyOne = false;
-                } else {
-                    //currencyOneTextView.setText(R.string.Dollars);
-                    //currencyTwoTextView.setText(R.string.Euros);
-                    isDollarsCurrencyOne = true;
+        currencyOneEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    convert(currencyOneSpinner, currencyTwoSpinner, currencyOneEditText, currencyTwoEditText);
+                    return true;
                 }
+                return false;
             }
-
-        });*/
+        });
 
         convertButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v){
-                String currencyOne = currencyOneSpinner.getSelectedItem().toString();
-                String currencyTwo = currencyTwoSpinner.getSelectedItem().toString();
-                double amount = Double.parseDouble(currencyOneEditText.getText().toString());
-
-                Locator loc = new Locator(currencyOne, currencyTwo, amount);
-
-                Thread request = new Thread(loc);
-
-                request.start();
-                try {
-                    request.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                currencyTwoEditText.setText(String.format(java.util.Locale.US, "%.2f", loc.getConversion()));
+            public void onClick(View v) {
+                convert(currencyOneSpinner, currencyTwoSpinner, currencyOneEditText, currencyTwoEditText);
             }
         });
     }
